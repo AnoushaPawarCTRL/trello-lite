6a3c46fcc1b4d177db39ae8004f98af140d43818
Anousha Pawar <anoushapawar1@gmail.com>
2026-06-30 15:16:12 +0100

    Hid password field from all JSON responses using JsonIgnore

--- src/main/java/com/example/demo/entity/User.java
+++ src/main/java/com/example/demo/entity/User.java
@@ -1,5 +1,7 @@
 package com.example.demo.entity;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
@@ -24,5 +26,6 @@ public class User {
     private String email;
 
     @Column(nullable = false)
+    @JsonIgnore
     private String password;
 }
